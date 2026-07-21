23fd4d6d5c8d8180ce43bbbe2c4006bd340139f8
Chowdhury Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2026-07-21 17:07:41 +0600

    fix: correct Filament Action namespace for repeater extra actions

--- app/Support/PageBlocks.php
+++ app/Support/PageBlocks.php
@@ -2,7 +2,7 @@
 
 namespace App\Support;
 
-use Filament\Forms\Components\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\FileUpload;
