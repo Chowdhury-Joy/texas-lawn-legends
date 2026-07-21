a7816a3397068acaa8ea888a42f0b8917ff385df
Chowdhury Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2026-07-21 17:28:20 +0600

    style: make TopAccountWidget span full width on dashboard

--- app/Filament/Widgets/TopAccountWidget.php
+++ app/Filament/Widgets/TopAccountWidget.php
@@ -7,4 +7,6 @@ use Filament\Widgets\AccountWidget as BaseAccountWidget;
 class TopAccountWidget extends BaseAccountWidget
 {
     protected static ?int $sort = -10;
+
+    protected int|string|array $columnSpan = 'full';
 }
